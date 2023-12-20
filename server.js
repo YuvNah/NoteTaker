4e96e4aef3e1792ac75045f2ca431da730d7ac21
Yuval Nahmias <yuvalit789@gmail.com>
2023-12-20 16:17:03 -0500

    first step to modulerize

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const fs = require("fs");
 
+const router = express.Router();
+
 // Import built-in Node.js package 'path' to resolve path of files that are located on the server
 const path = require("path");
 
@@ -18,34 +20,29 @@ const PORT = 3001;
 // Static middleware pointing to the public folder
 app.use(express.static("public"));
 
-// Create Express.js routes for default '/', '/send' and '/routes' endpoints
-// app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
+// app.get("/notes", (req, res) =>
+//   res.sendFile(path.join(__dirname, "public/notes.html"))
+// );
 
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/notes.html"))
-);
+// app.get("/api/notes", (req, res) =>
+//   res.sendFile(path.join(__dirname, "db/db.json"))
+// );
 
-app.get("/api/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "db/db.json"))
-);
-
-app.post("/api/notes", (req, res) => {
-  // console.log(req.body);
-  fs.readFile("db/db.json", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(req.body);
-      console.log(parsedData);
-      fs.writeFile("db/db.json", JSON.stringify(parsedData), (err) =>
-        err ? console.error(err) : res.json()
-      );
-    }
-  });
-});
-
-app.get("*", (req, res) => res.send("Navigate to /send or /routes"));
+// app.post("/api/notes", (req, res) => {
+//   // console.log(req.body);
+//   fs.readFile("db/db.json", "utf8", (err, data) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       const parsedData = JSON.parse(data);
+//       parsedData.push(req.body);
+//       console.log(parsedData);
+//       fs.writeFile("db/db.json", JSON.stringify(parsedData), (err) =>
+//         err ? console.error(err) : res.json()
+//       );
+//     }
+//   });
+// });
 
 // listen() method is responsible for listening for incoming connections on the specified port
 app.listen(PORT, () =>
